9a05d31ec93ce4c67f7b041506fa6f6b609ea26a
Meghana Tetala <meghana.tetala@winwire.com>
2026-08-11 15:49:23 +0530

    Change Name of Employee

--- empSystem/Program.cs
+++ empSystem/Program.cs
@@ -5,7 +5,7 @@
         var meg = new Meghana();
         meg.MeghanaDisplay();
 
-        var emp =  new Employee(101,"shodhan","SDT",50000);
+        var emp =  new Employee(105,"meghana","SDT",50000);
         emp.DisplayInfo();
     }
 }
