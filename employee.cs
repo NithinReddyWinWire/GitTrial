509ac9ed335663a4d65ef60e76709fdc0ef22b32
Shodhan <shodhan.vemulapalli@winwire.com>
2026-08-11 12:55:09 +0530

    added employee

---
+++ employee.cs
@@ -0,0 +1,20 @@
+public class employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public decimal Salary { get; set; }
+
+    public employee(int id, string name, string position, decimal salary)
+    {
+        Id = id;
+        Name = name;
+        Position = position;
+        Salary = salary;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"ID: {Id}, Name: {Name}, Position: {Position}, Salary: {Salary:C}");
+    }
+}
