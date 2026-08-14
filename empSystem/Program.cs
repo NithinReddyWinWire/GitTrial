c4ba09b4bc4bbd9c7ae1721622580f16a6bb5998
NithinReddyWinWire <nithin.reddy@WinWire.com>
2026-08-14 16:48:55 +0530

    Add test output to Program.cs

--- empSystem/Program.cs
+++ empSystem/Program.cs
@@ -5,6 +5,8 @@
         var meg = new Meghana();
         meg.MeghanaDisplay();
 
+        Console.writeLine("Testing!");
+
         var emp =  new Employee(1055,"nithin","SDT",50000);
         emp.DisplayInfo();
     }
